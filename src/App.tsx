bd43de4253287e16ad2d22cf63c9c2095440cd9c
Marco Arruda <marco.nc.arruda@gmail.com>
2022-01-06 09:36:17 -0400

    Create components for connector and connection status

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Connector from "./components/Connector";
+import ConnectionStatus from "./components/ConnectionStatus"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="/" onClick={event => event.preventDefault()}>ROSWeb 3.0</a>
+        <a href="/" onClick={(event) => event.preventDefault()}>
+          ROSWeb 3.0
+        </a>
+        <div style={{ flexShrink: 1 }}>
+          <ConnectionStatus />
+        </div>
+        <div style={{ flexGrow: 1 }}>
+          <Connector />
+        </div>
       </header>
       <section className="App-section"></section>
     </div>
---
+++ src/components/ConnectionStatus.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ConnectionStatus() {
+  return (
+    <div style={{ padding: '0 20px' }}>
+      <span style={{ color: "green" }}>On</span>
+      <span style={{ color: "red" }}>Off</span>
+    </div>
+  );
+}
+
+export default ConnectionStatus;
